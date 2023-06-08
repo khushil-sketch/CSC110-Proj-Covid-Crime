346b04b5ab2410ab42be2c21c3ecfdf7d79b0d3d
Khushil Nagda <52947378+khushil-sketch@users.noreply.github.com>
2023-06-08 12:12:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def canada_covid_crime_line() -> None:
     status = tk.Label(root, text="Graph Successfully Plotted", font=paragraph_font, bg="white")
     status.grid(row=2, column=4, sticky="WE")
 
+    plt.show()
 
 graph_button_1 = tk.Button(root, text="Covid Cases & Crime Rate against Time", command=canada_covid_crime_line, padx=10,
                            pady=10, bg="#47aeed", fg="white", font=button_font)
@@ -62,14 +63,14 @@ def physical_crime_scatter() -> None:
     physical_covid_canada_20_21 = plt.figure()
     physical_covid_canada_20_21_axes = physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
     physical_crimes_20_21 = df.total_police_data_canada_20_21_physical_df['Total Physical Crimes in Canada']
-    physical_covid_canada_20_21_axes.scatter(cases_20_21, physical_crimes_20_21.iloc[0:20])
+    physical_covid_canada_20_21_axes.scatter(cases_20_21, physical_crimes_20_21.iloc[0:20], label='Data')
 
     # Stat Analysis and Regression Plot
     physical_model = np.polyfit(cases_20_21, physical_crimes_20_21.iloc[0:20], 1)
     physical_predict = np.poly1d(physical_model)
     x_lin_reg = range(cases_20_21.min(), cases_20_21.max())
     y_lin_reg = physical_predict(x_lin_reg)
-    physical_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r')
+    physical_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r', label='Regression')
 
     physical_r = round(np.corrcoef(cases_20_21, physical_crimes_20_21.iloc[0:20])[0, 1], 7)
 
@@ -83,6 +84,7 @@ def physical_crime_scatter() -> None:
     status = tk.Label(root, text="Graph Successfully Plotted", font=paragraph_font)
     status.grid(row=4, column=4, sticky="WE")
 
+    plt.show()
 
 graph_button_2 = tk.Button(root, text="Covid Cases vs Physical Crime Cases", command=physical_crime_scatter, padx=10,
                            pady=10, bg="#cf7ce6", fg="white", font=button_font)
@@ -97,14 +99,14 @@ def non_physical_crime_scatter() -> None:
     non_physical_covid_canada_20_21_axes = non_physical_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
     non_physical_crimes_20_21 = \
         df.total_police_data_canada_20_21_non_physical_df['Total Non Physical Crimes in Canada']
-    non_physical_covid_canada_20_21_axes.scatter(cases_20_21, non_physical_crimes_20_21.iloc[0:20])
+    non_physical_covid_canada_20_21_axes.scatter(cases_20_21, non_physical_crimes_20_21.iloc[0:20], label='Data')
 
     # Stat Analysis and Regression Plot
     non_physical_model = np.polyfit(cases_20_21, non_physical_crimes_20_21.iloc[0:20], 1)
     non_physical_predict = np.poly1d(non_physical_model)
     x_lin_reg = range(cases_20_21.min(), cases_20_21.max())
     y_lin_reg = non_physical_predict(x_lin_reg)
-    non_physical_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r')
+    non_physical_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r', label='Regression')
 
     non_physical_r = round(np.corrcoef(cases_20_21, non_physical_crimes_20_21.iloc[0:20])[0, 1], 7)
 
@@ -118,6 +120,7 @@ def non_physical_crime_scatter() -> None:
     status = tk.Label(root, text="Graph Successfully Plotted", font=paragraph_font, bg='white')
     status.grid(row=6, column=4, sticky="WE")
 
+    plt.show()
 
 graph_button_3 = tk.Button(root, text="Covid Cases vs Non Physical Crime Cases", command=non_physical_crime_scatter,
                            padx=10, pady=10, bg="#84cc6c", fg="white", font=button_font)
@@ -132,14 +135,14 @@ def public_crime_scatter() -> None:
     public_covid_canada_20_21_axes = public_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
     public_crimes_20_21 = \
         df.total_police_data_canada_20_21_public_df['Total Public Crimes in Canada']
-    public_covid_canada_20_21_axes.scatter(cases_20_21, public_crimes_20_21.iloc[0:20])
+    public_covid_canada_20_21_axes.scatter(cases_20_21, public_crimes_20_21.iloc[0:20], label='Data')
 
     # Stat Analysis and Regression Plot
     public_model = np.polyfit(cases_20_21, public_crimes_20_21.iloc[0:20], 1)
     public_predict = np.poly1d(public_model)
     x_lin_reg = range(cases_20_21.min(), cases_20_21.max())
     y_lin_reg = public_predict(x_lin_reg)
-    public_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r')
+    public_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r', label='Regression')
 
     public_r = round(np.corrcoef(cases_20_21, public_crimes_20_21.iloc[0:20])[0, 1], 7)
 
@@ -153,6 +156,7 @@ def public_crime_scatter() -> None:
     status = tk.Label(root, text="Graph Successfully Plotted", font=paragraph_font)
     status.grid(row=8, column=4, sticky="WE")
 
+    plt.show()
 
 graph_button_4 = tk.Button(root, text="Covid Cases vs Public Crime Cases", command=public_crime_scatter,
                            padx=10, pady=10, bg="#de9440", fg="white", font=button_font)
@@ -167,14 +171,14 @@ def private_crime_scatter() -> None:
     private_covid_canada_20_21_axes = private_covid_canada_20_21.add_axes([0.1, 0.1, 0.8, 0.8])
     private_crimes_20_21 = \
         df.total_police_data_canada_20_21_private_df['Total Private Crimes in Canada']
-    private_covid_canada_20_21_axes.scatter(cases_20_21, private_crimes_20_21.iloc[0:20])
+    private_covid_canada_20_21_axes.scatter(cases_20_21, private_crimes_20_21.iloc[0:20], label='Data')
 
     # Stat Analysis and Regression Plot
     private_model = np.polyfit(cases_20_21, private_crimes_20_21.iloc[0:20], 1)
     private_predict = np.poly1d(private_model)
     x_lin_reg = range(cases_20_21.min(), cases_20_21.max())
     y_lin_reg = private_predict(x_lin_reg)
-    private_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r')
+    private_covid_canada_20_21_axes.plot(x_lin_reg, y_lin_reg, c='r', label='Regression')
 
     private_r = round(np.corrcoef(cases_20_21, private_crimes_20_21.iloc[0:20])[0, 1], 7)
     private_covid_canada_20_21_axes.set_title("Covid -> Total Private Crime (r=" + str(private_r) + ')')
@@ -189,6 +193,7 @@ def private_crime_scatter() -> None:
     status = tk.Label(root, text="Graph Successfully Plotted", font=paragraph_font, bg='white')
     status.grid(row=10, column=4, sticky="WE")
 
+    plt.show()
 
 graph_button_5 = tk.Button(root, text="Covid Cases vs Private Crime Cases", command=private_crime_scatter,
                            padx=10, pady=10, bg="#de5240", fg="white", font=button_font)
